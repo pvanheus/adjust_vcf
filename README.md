3434acfe674bcdfde2c6fac693309ef1d93b203f
Peter van Heusden <pvanheusden@uwc.ac.za>
2023-09-21 22:43:12 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # adjust_vcf
+
+Scripts to adjust coordinates in VCF. The adjustements are specified in a BED-like file with the following files:
+
+dest_chrom start_coord end_coord adjustment src_chrom
+
+where:
+
+* dest_chrom is the destination chromosome (or sequence) name
+* start_coord is the starting coordinate of a block on the src chromosome
+* end_coord is the ending coordinate (half-open, i.e. the base is not included) of a segment on the src chromosome
+* offset is the offset to convert coordinates in this segment from src to destination coordinates
+* src_chrom is the name of the src chromosome (or sequence)
+
+The `psl_to_bed.py` script computes this format output from [PSL format](http://www.ensembl.org/info/website/upload/psl.html) input.
+
+The `adjust_vcf.py` uses the BED-like format to convert VCF from src_chrom to dest_chrom. It is assumed that src_chrom to dest_chrom is a 1-to-1 mapping. This script requires the [intervaltree](https://github.com/chaimleib/intervaltree/tree/master) Python module.
+
+These scripts are very rough and might contain bugs.
---
+++ adjust_vcf.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import argparse
+import sys
+
+from intervaltree import IntervalTree, Interval
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vcf_input', type=argparse.FileType())
+    parser.add_argument('bed_input', type=argparse.FileType())
+    parser.add_argument('vcf_output', type=argparse.FileType('w'), nargs='?', default=sys.stdout)
+    args = parser.parse_args()
+
+    adjustment_intervals = IntervalTree()
+    adjustment_trees = {}
+    for line in args.bed_input:
+        if line.startswith('#'):
+            continue
+        (dest_chrom, start_str, end_str, offset_str, src_chrom) = line.strip().split('\t')
+        if src_chrom not in adjustment_trees:
+            adjustment_trees[src_chrom] = {}
+        if dest_chrom not in adjustment_trees[src_chrom]:
+            adjustment_trees[src_chrom][dest_chrom] = IntervalTree()
+            assert len(adjustment_trees[src_chrom]) == 1, "Error: currently only a single src chromosome to destination chromosome mapping is supported"
+        start = int(start_str)
+        end = int(end_str)
+        offset = int(offset_str)
+        interval = Interval(start, end, offset)
+        adjustment_trees[src_chrom][dest_chrom].add(interval)
+    
+    for line in args.vcf_input:
+        if line.startswith('#'):
+            args.vcf_output.write(line)
+            continue
+        fields = line.split('\t')
+        src_chrom = fields[0]
+        (dest_chrom, adjustment_intervals) = list(adjustment_trees[src_chrom].items())[0]  # we ensure that there is only one item by the assert above
+        pos = int(fields[1]) - 1  # VCF is 1-based, BED is 0-based
+        if adjustment_intervals.overlaps(pos):
+            intervals = list(adjustment_intervals[pos])
+            assert len(intervals) == 1
+            offset = intervals[0].data
+            pos += offset
+        pos += 1
+        fields[0] = dest_chrom
+        fields[1] = str(pos)
+        args.vcf_output.write('\t'.join(fields))
